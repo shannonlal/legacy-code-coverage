8742daea7161b60230ffdf7f1a97abc8319749a8
Shannon Lal <shannonlal@hotmail.com>
2019-12-11 20:41:25 -0500

    Frist sample project

---
+++ src/count.js
@@ -0,0 +1,12 @@
+/**
+ * The following function will return the addition of the parameters
+ * @param {number} a 
+ * @param {number} b 
+ */
+const add = (a,b) => {
+    return a+b;
+}
+
+module.exports = {
+    add
+};
